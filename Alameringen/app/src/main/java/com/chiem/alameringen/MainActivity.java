afce0d375e614e45e2f7819e4e05207ceb1e5298
Chiem <Chiem.stevens@outlook.com>
2019-12-11 12:27:28 +0100

    Alarmeringen: Added working menu icons

--- Alameringen/app/src/main/java/com/chiem/alameringen/MainActivity.java
+++ Alameringen/app/src/main/java/com/chiem/alameringen/MainActivity.java
@@ -1,18 +1,12 @@
 package com.chiem.alameringen;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,18 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.action_recents:
+                    case R.id.action_map:
                         Toast.makeText(MainActivity.this, "Recents", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.action_favorites:
+                    case R.id.action_place:
                         Toast.makeText(MainActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
                         break;
-                    case R.id.action_nearby:
+                    case R.id.action_emergency:
                         Toast.makeText(MainActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
                         break;
                 }
